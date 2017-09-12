a5672a82f41ce0e957bcd5c8a344b867b97d806a
Brad Oyler <bradoyler@gmail.com>
2017-09-12 17:53:54 -0400

    hash router

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,61 @@ export default {
 </script>
 
 <style>
-body {
-  margin: 0;
+html, body {
+    position: relative;
+    height: 100%;
 }
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+    background: #eee;
+    font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+    color: #000;
+    margin: 0;
+    padding: 0;
+}
+.swiper-container {
+    width: 375px;
+    max-height: 667px;
+}
+.swiper-slide {
+  position: relative;
+  height: 100%;
+  background: #fff;
+}
+
+.story {
+  position: absolute;
+}
+
+.story > h1, h2, h3, h4 {
+  color: #fff;
+  padding: 12px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+}
+
+.story > .view-story {
+  border: 0;
+  background-color: transparent;
+  color: #fff;
+  position: fixed;
+  bottom: 15px;
+  font-size: 1.5em;
+  cursor: pointer;
+  width: 100%;
+}
+
+.swiper-button-next {
+  top: 30px;
+  background-color: transparent;
+  height: 100%;
+  width: 50%;
+}
+
+.swiper-button-prev {
+  top: 30px;
+  background-color: transparent;
+  height: 100%;
+  width: 50%;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,47 +1,47 @@
 <template>
-      <div class="subhead">
-        <!-- swiper -->
-        <swiper :options="swiperOption">
-        <swiper-slide data-hash="story/1" >
-         <div class="story">
-             <h1>Story 1 headline</h1>
-             <h3>Some kind of summary text...</h3>
-             <button class="view-story" type="button" name="button">View Story 1</button>
-         </div>
-        <img src="https://lorempixel.com/375/667/nature/1" alt="">
-       </swiper-slide>
-       <swiper-slide class="swiper-slide" data-hash="story/2" >
-         <div class="story">
-             <h1>Story 2 headline</h1>
-             <button class="view-story" type="button" name="button">View Story 2</button>
-         </div>
-         <img src="https://lorempixel.com/375/667/nature/2" alt="">
-       </swiper-slide>
-       <swiper-slide class="swiper-slide" data-hash="story/3" >
-         <div class="story">
-             <h1>Story 3 headline</h1>
-             <button class="view-story" type="button" name="button">View Story 3</button>
-         </div>
-         <img src="https://lorempixel.com/375/667/nature/3" alt="">
-       </swiper-slide>
-       <swiper-slide class="swiper-slide" data-hash="story4" >
-         <div class="story">
-             <h1>Story 4 headline</h1>
-             <button class="view-story" type="button" name="button">View Story 4</button>
-         </div>
-         <img src="https://lorempixel.com/375/667/nature/4" alt="">
-       </swiper-slide>
-       <swiper-slide class="swiper-slide" data-hash="story5" >
-         <div class="story">
-             <h1>Story 5 headline</h1>
-             <input type="text" name="" value="">
-             <button type="button" name="button">subscribe</button>
-         </div>
-         <img src="https://lorempixel.com/375/667/nature/5" alt="">
-       </swiper-slide>
-          <div class="swiper-pagination" slot="pagination"></div>
-        </swiper>
-      </div>
+  <div class="subhead">
+    <!-- swiper -->
+    <swiper :options="swiperOption">
+    <swiper-slide data-hash="story/1" >
+     <div class="story">
+         <h1>Story 1 headline</h1>
+         <h3>Some kind of summary text...</h3>
+         <button class="view-story" type="button" name="button">View Story 1</button>
+     </div>
+    <img src="https://lorempixel.com/375/667/nature/1" alt="">
+   </swiper-slide>
+   <swiper-slide class="swiper-slide" data-hash="story/2" >
+     <div class="story">
+         <h1>Story 2 headline</h1>
+         <button class="view-story" type="button" name="button">View Story 2</button>
+     </div>
+     <img src="https://lorempixel.com/375/667/nature/2" alt="">
+   </swiper-slide>
+   <swiper-slide class="swiper-slide" data-hash="story/3" >
+     <div class="story">
+         <h1>Story 3 headline</h1>
+         <button class="view-story" type="button" name="button">View Story 3</button>
+     </div>
+     <img src="https://lorempixel.com/375/667/nature/3" alt="">
+   </swiper-slide>
+   <swiper-slide class="swiper-slide" data-hash="story4" >
+     <div class="story">
+         <h1>Story 4 headline</h1>
+         <button class="view-story" type="button" name="button">View Story 4</button>
+     </div>
+     <img src="https://lorempixel.com/375/667/nature/4" alt="">
+   </swiper-slide>
+   <swiper-slide class="swiper-slide" data-hash="story5" >
+     <div class="story">
+         <h1>Story 5 headline</h1>
+         <input type="text" name="" value="">
+         <button type="button" name="button">subscribe</button>
+     </div>
+     <img src="https://lorempixel.com/375/667/nature/5" alt="">
+   </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
 </template>
 
 <script>
@@ -56,6 +56,11 @@
           nextButton: '.swiper-button-next',
           prevButton: '.swiper-button-prev',
           spaceBetween: 30,
+          onSlideChangeEnd: swiper => {
+            console.log('onSlideChangeEnd - index:', swiper.realIndex, swiper)
+            const idx = swiper.realIndex + 1
+            history.pushState({}, `page ${idx}`, `#/story/${idx}`)
+          },
           // hashnav: true,
           hashnavWatchState: true,
           keyboardControl: true
